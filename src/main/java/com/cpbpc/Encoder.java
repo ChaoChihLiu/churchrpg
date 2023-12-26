b495300813a07520ac51b295916435aa21729052
Liu ChaoChih <chaochihliu@gmail.com>
2023-12-26 15:03:23 +0800

    bug fix: 哥林多前书十章1-14节

--- src/main/java/com/cpbpc/Encoder.java
+++ src/main/java/com/cpbpc/Encoder.java
@@ -2,12 +2,16 @@ package com.cpbpc;
 
 import com.github.houbb.opencc4j.util.ZhConverterUtil;
 
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 
 public class Encoder {
 
     public static  void main(String args[]){
-        System.out.println(URLEncoder.encode(ZhConverterUtil.toSimple("士1-3")));
+        System.out.println(URLEncoder.encode(ZhConverterUtil.toSimple("歌林多前書")));
+        System.out.println(URLEncoder.encode(ZhConverterUtil.toSimple("歌林多後書")));
+
+        System.out.println(URLDecoder.decode("%E6%9E%97%E5%89%8D"));
     }
 
 }
