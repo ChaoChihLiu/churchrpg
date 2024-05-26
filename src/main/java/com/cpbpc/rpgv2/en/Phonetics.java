c65d19a0cafaf726a05843a5f8604f71b16fdfa7
Liu ChaoChih <chaochihliu@gmail.com>
2024-05-26 15:32:22 +0800

    phonetics db set is huge, split to sub-set and change text

--- src/main/java/com/cpbpc/rpgv2/en/Phonetics.java
+++ src/main/java/com/cpbpc/rpgv2/en/Phonetics.java
@@ -6,6 +6,7 @@ import com.cpbpc.comms.PunctuationTool;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -34,32 +35,29 @@ public class Phonetics implements PhoneticIntf {
         ([\s|&nbsp;]{1,}overlived[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Machpelah[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Lahairoi[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Chessed[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Jidlaph[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Mamre[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Gihon[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Pison[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Jehovahjireh[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Girgashites[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}girgashites[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Methusael[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Habakkuk[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}villany[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Malachi[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Phichol[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}covert[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}triest[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Cainan[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}sware[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}lieth[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Jabal[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Jubal[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Laban[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Raca[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Irad[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Enos[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Hazo[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}err[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}ERR[\s|&nbsp;]{1,}|[\s|&nbsp;]{1,}Nun[\s|&nbsp;]{1,})
          */
 
-        String p = generatePattern();
-        logger.info(p);
-        Pattern r = Pattern.compile(p);
-        Matcher matcher = r.matcher(content);
-
-        List<String> finds = new ArrayList<>();
-        while (matcher.find()) {
-            finds.add(matcher.group(1));
-        }
-        logger.info("what is my finds : " + finds.toString());
-
+        List<String> patterns = generatePattern();
         String replaced = content;
-        for (String key : finds) {
-            key = StringUtils.trim(key);
-            String completeForm = lookupCompleteForm(key);
-            logger.info("complete form " + completeForm);
-            if (phonetic.get(key) != null && phonetic.get(key).getPaused()) {
-                replaced = replaced.replace(" " +key+" ", " " + completeForm + " " + "[pause]") ;
-                replaced = replaceWithAllowedPunc(replaced, key,  completeForm, true);
-//                        .replace(" " +key+",", " " + completeForm + "," + "[pause]")
-//                        .replace(" " +key+".", " " + completeForm + "." + "[pause]");
-            } else {
-                replaced = replaced.replace(" " +key+" ", " " + completeForm + " ")  ;
-//                                    .replace(" " +key+",", " " + completeForm + ",")
-//                                    .replace(" " +key+".", " " + completeForm + ".")
-                replaced = replaceWithAllowedPunc(replaced, key,  completeForm, false);
+        for( String p : patterns ){
+            logger.info(p);
+            Pattern r = Pattern.compile(p);
+            Matcher matcher = r.matcher(content);
+
+            List<String> finds = new ArrayList<>();
+            while (matcher.find()) {
+                finds.add(matcher.group(1));
+            }
+            logger.info("what is my finds : " + finds.toString());
+            for (String key : finds) {
+                key = StringUtils.trim(key);
+                String completeForm = lookupCompleteForm(key);
+                logger.info("complete form " + completeForm);
+                if (phonetic.get(key) != null && phonetic.get(key).getPaused()) {
+                    replaced = replaced.replace(" " +key+" ", " " + completeForm + " " + "[pause]") ;
+                    replaced = replaceWithAllowedPunc(replaced, key,  completeForm, true);
+                } else {
+                    replaced = replaced.replace(" " +key+" ", " " + completeForm + " ")  ;
+                    replaced = replaceWithAllowedPunc(replaced, key,  completeForm, false);
+                }
             }
         }
 
@@ -130,23 +128,39 @@ public class Phonetics implements PhoneticIntf {
 
      */
 
-    private static String generatePattern() {
-
-//        return "([\\s{1,}|,{1,}](e\\.{0,}g\\.{0,}|i\\.{0,}e\\.{0,}|etc\\.{0,}))([\\s{1,}|,{1,}])";
-        StringBuilder builder = new StringBuilder("(");
-
-        Set<String> keySet = phonetic.keySet();
-        for (String key : keySet) {
-//            String newKey = key.trim().replace(".", "\\.{0,}");
-            String newKey = key.replace(".", "\\.");
-            builder.append("(&nbsp;|\\s){1,}").append(newKey).append("(&nbsp;|\\s){1,}|")
-            ;
-        }
-        if (builder.toString().endsWith("|")) {
-            builder.delete(builder.length() - 1, builder.length());
+    private static List<List<String>> splitSet(Set<String> set){
+        String[] arrayFromSet = set.toArray(new String[0]);
+        List<List<String>> list = new ArrayList<>();
+        int batchSize = 100;
+        for (int i = 0; i < arrayFromSet.length; i += batchSize) {
+            int endIndex = Math.min(i + batchSize, arrayFromSet.length);
+            String[] subArray = Arrays.copyOfRange(arrayFromSet, i, endIndex);
+            list.add( List.of(subArray) );
         }
 
-        builder.append(")");
-        return builder.toString();
+        return list;
     }
+
+    private static List<String> generatePattern() {
+        List<String> patterns = new ArrayList<>();
+
+        List<List<String>> keySet = splitSet(phonetic.keySet());
+
+        for (List<String> keys : keySet) {
+            StringBuilder builder = new StringBuilder("(");
+            for( String key: keys ){
+                String newKey = key.replace(".", "\\.");
+                builder.append("(&nbsp;|\\s){1,}").append(newKey).append("(&nbsp;|\\s){1,}|")
+                ;
+            }
+            if (builder.toString().endsWith("|")) {
+                builder.delete(builder.length() - 1, builder.length());
+            }
+            builder.append(")");
+            patterns.add(builder.toString());
+        }
+
+        return patterns;
+    }
+
 }
